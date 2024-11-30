ba48c1c49da229f1732858c7cdfdd5cff44aba59
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2024-11-30 02:37:37 -0300

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,26 @@
-#include <RE/Skyrim.h>
-#include <SKSE/SKSE.h>
-#include <format>
+#include "logger.h"
+struct OurEventSink : public RE::BSTEventSink<RE::TESHitEvent> {
+    RE::BSEventNotifyControl ProcessEvent(const RE::TESHitEvent* event, RE::BSTEventSource<RE::TESHitEvent>*) {
+        
+        auto targetName  = event->target->GetBaseObject()->GetName();
 
-// Função para tratar teclas pressionadas
-void OnKeyPress(uint32_t keyCode) {
-    auto message = std::format("Key Pressed: {}", keyCode);
-    RE::DebugNotification(message.c_str());
-}
+        auto sourceName = event->cause->GetBaseObject()->GetName();
 
-// Classe para manipular eventos de entrada
-class InputEventHandler : public RE::BSTEventSink<RE::InputEvent> {
-public:
-    // Implementação correta do método ProcessEvent
-    virtual RE::BSEventNotifyControl ProcessEvent(RE::InputEvent** a_event,
-                                                  RE::BSTEventSource<RE::InputEvent*>* a_eventSource) override {
-        if (!a_event || !*a_event) {
-            return RE::BSEventNotifyControl::kContinue;
-        }
-
-        // Itera pelos eventos fornecidos
-        for (RE::InputEvent* inputEvent = *a_event; inputEvent; inputEvent = inputEvent->next) {
-            // Verifica se o evento é do tipo ButtonEvent
-            if (const auto* buttonEvent = inputEvent->AsButtonEvent()) {
-                if (buttonEvent->IsDown()) {  // Checa se a tecla foi pressionada
-                    uint32_t keyCode = buttonEvent->GetIDCode();
-                    OnKeyPress(keyCode);
-                }
-            }
-        }
+        logger
 
         return RE::BSEventNotifyControl::kContinue;
     }
-
-    // Destrutor virtual para evitar problemas de memória
-    virtual ~InputEventHandler() = default;
 };
 
-// Função principal para carregar o plugin
-bool SKSEPluginLoad(const SKSE::LoadInterface* skse) {
+
+SKSEPluginLoad(const SKSE::LoadInterface *skse) {
     SKSE::Init(skse);
 
-    auto inputHandler = new InputEventHandler();
-    RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::InputEvent>(inputHandler);
+    RE::ScriptEventSourceHolder* eventSourceHolder = RE::ScriptEventSourceHolder::GetSingleton();
+
+
+    eventSourceHolder->AddEventSink
+   
 
     return true;
 }
